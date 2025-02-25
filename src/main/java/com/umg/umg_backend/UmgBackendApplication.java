590e20d80f9edd22ea87e0bb7ac75f13b3467016
Flavio Barbosa <flaviombarbosa@gmail.com>
2025-02-25 17:16:27 -0300

    initial commit

---
+++ src/main/java/com/umg/umg_backend/UmgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.umg.umg_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmgBackendApplication.class, args);
+	}
+
+}
