f3060ef6a4426e9e1372ee1cfc014be9b265c9ad
Flavio Barbosa <flaviombarbosa@gmail.com>
2025-02-26 07:44:59 -0300

    add basic authentication

--- src/main/java/com/umg/umg_backend/TestController.java
+++ src/main/java/com/umg/umg_backend/TestController.java
@@ -1,22 +1,19 @@
 package com.umg.umg_backend;
 
-import com.umg.umg_backend.domain.model.SpotifyAccessToken;
-import com.umg.umg_backend.domain.service.SpotifyService;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/health")
 public class TestController {
 
-  @Autowired
-  private SpotifyService spotifyService;
-
   @GetMapping
+  @ResponseStatus(HttpStatus.OK)
   public String getSpotifyAccessToken() {
-    SpotifyAccessToken accessToken = spotifyService.getAccessToken();
-    return accessToken.getAccessToken();
+    return "Ok @ " + LocalDateTime.now().toString();
   }
 }
---
+++ src/main/java/com/umg/umg_backend/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.umg.umg_backend.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(urlRegistry -> urlRegistry.anyRequest().authenticated())
+        .formLogin(withDefaults())
+        .httpBasic(withDefaults());
+
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails user = User.withUsername("umg").password("{noop}umg").build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+}
